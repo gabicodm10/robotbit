8ad4e7d1574ffde55060bcfa567a4a1a41902a1c
gabicodm10 <gabriel.croquet@etik.com>
2025-04-14 10:46:12 +0200

    activation des capteur

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 def on_received_value(name, value):
-    global x, y
-    if "x" == name:
-        x = value / 4
-    if "y" == name:
-        y = value / 4
+    global robotonoff
+    if "onoff" == name:
+        if value == 1:
+            basic.show_icon(IconNames.YES)
+            robotonoff = 1
 radio.on_received_value(on_received_value)
 
-x = 0
-y = 0
+robotonoff = 0
+robotonoff = 0
 basic.show_leds("""
+    . # . # .
+    . . . . .
     . . # . .
     . # # # .
-    # # # # #
-    . # . # .
-    . # . # .
+    . . . . .
     """)
 y = 0
 x = 0
 radio.set_group(5)
 
 def on_forever():
-    robotbit.motor_run_dual(robotbit.Motors.M1A, y + x, robotbit.Motors.M1A, y - x)
+    if pins.digital_read_pin(DigitalPin.P14) == 1:
+        basic.show_icon(IconNames.BUTTERFLY)
+        robotbit.motor_stop_all()
+        basic.pause(500)
+    if robotonoff == 1:
+        robotbit.motor_run_dual(robotbit.Motors.M1A, y + x, robotbit.Motors.M2A, y - x)
+    if robotonoff == 0:
+        robotbit.motor_run_dual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
 basic.forever(on_forever)
-
--- main.ts
+++ main.ts
@@ -1,25 +1,17 @@
 radio.onReceivedValue(function (name, value) {
     if ("onoff" == name) {
         if (value == 1) {
+            basic.showIcon(IconNames.Yes)
             robotonoff = 1
         }
     }
-    if ("onoff" == name) {
-        if (value == 0) {
-            robotonoff = 0
-        }
-    }
-    if ("x" == name) {
-        x = value / 4
-    }
-    if ("y" == name) {
-        y = value / 4
-    }
 })
 let robotonoff = 0
-let x = 0
-let y = 0
-let onoff = 0
+let capD = DigitalPin.P14
+pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+let capG = DigitalPin.P13
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+robotonoff = 0
 basic.showLeds(`
     . # . # .
     . . . . .
@@ -27,11 +19,26 @@ basic.showLeds(`
     . # # # .
     . . . . .
     `)
-y = 0
-x = 0
+let y = 0
+let x = 0
 radio.setGroup(5)
 basic.forever(function () {
-    if (("robotonoff" as any) == ("1" as any)) {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showString("G")
+    } else {
+        basic.clearScreen()
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showString("D")
+    } else {
+        basic.clearScreen()
+    }
+    if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        basic.showIcon(IconNames.Butterfly)
+        robotbit.MotorStopAll()
+        basic.pause(500)
+    }
+    if (robotonoff == 1) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         y + x,
@@ -39,7 +46,7 @@ basic.forever(function () {
         y - x
         )
     }
-    if (("robotonoff" as any) == ("0" as any)) {
+    if (robotonoff == 0) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
         0,
